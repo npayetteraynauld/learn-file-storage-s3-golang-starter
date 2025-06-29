f195ad050746a3e11082683327680f779f303742
nathiboi <npayetteraynauld@hotmail.com>
2025-06-29 16:21:37 -0400

    day3

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,8 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
+	"path/filepath"
+	"os"
+	"mime"
+	"crypto/rand"
+	"encoding/base64"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -33,5 +40,90 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+	return 
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+
+	/*dat, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read image file", err)
+		return
+	}*/
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to get video", err)
+		return 
+	}
+
+	if video.CreateVideoParams.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", nil)
+		return
+	}	
+
+	mediaType, _, _ := mime.ParseMediaType(contentType)
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Not a valid image format", nil)
+		return 
+	}
+	
+	exts, _ := mime.ExtensionsByType(contentType)
+	ext := ".bin"
+	if len(exts) > 0 {
+		ext = exts[0] 
+	}
+
+	byteSlice := make([]byte, 32)
+	_, err = rand.Read(byteSlice)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't generate random", err)
+		return
+	}
+	encoded := base64.RawURLEncoding.EncodeToString(byteSlice)
+
+	fileName := fmt.Sprintf("%v%v", encoded, ext)
+	videoFilePath := filepath.Join(cfg.assetsRoot, fileName)
+
+	newFile, err := os.Create(videoFilePath)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't create file", err)
+		return
+	}
+	defer newFile.Close()
+
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "couldn't copy file", err)
+		return 
+	}
+
+	newURL := fmt.Sprintf("http://localhost:%v/assets/%v", cfg.port, fileName)
+	video.ThumbnailURL = &newURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't update video", err)
+		return 
+	}
+
+	respondWithJSON(w, http.StatusOK, database.Video{
+		ID: video.ID,
+		CreatedAt: video.CreatedAt,
+		UpdatedAt: video.UpdatedAt,
+		ThumbnailURL: video.ThumbnailURL,
+		VideoURL: video.VideoURL,
+		CreateVideoParams: database.CreateVideoParams{
+			Title: video.CreateVideoParams.Title,
+			Description: video.CreateVideoParams.Description,
+			UserID: video.CreateVideoParams.UserID,
+		},
+	})
 }
--- handler_upload_video.go
+++ handler_upload_video.go
@@ -2,6 +2,114 @@ package main
 
 import (
 	"net/http"
+	"fmt"
+	"io"
+	"mime"
+	"os"
+	"crypto/rand"
+	"encoding/hex"
+	
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get video from db", err)
+		return 
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not permitted", err)
+		return
+	}
+	
+
+	fmt.Println("uploading video", videoID, "by user", userID)
+
+	const uploadLimit = 1 << 30
+	r.Body = http.MaxBytesReader(w, r.Body, uploadLimit)
+	defer r.Body.Close()
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return 
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+	
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to get media Type from header", err)
+		return
+	}
+
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Video isn't mp4", err)
+		return
+	}
+
+	temp, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create temp file", err)
+		return
+	}
+	defer os.Remove(temp.Name())
+	defer temp.Close()
+
+	_, err = io.Copy(temp, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't copy file", err)
+		return
+	}
+
+	_, err = temp.Seek(0, io.SeekStart
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could't change temp pointer", err)
+		return
+	}
+	
+	bytes := make([]byte, 16)
+	_, err = rand.Read(bytes)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "error reading random bytes", err)
+		return
+	}
+	
+	hexString := hex.EncodeToString(bytes)
+	fileKey := hexString + ".mp4"
+
+	_, err = cfg.s3Client.PutObject(context.TODO(), s3.PutObjectInput{
+		Bucket:      aws.String("tubely-77247"),
+		Key:         aws.String(hexString),
+		Body:        temp,
+		ContentType: mediaType,
+	})	
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "failed to upload file", err)
+		return 
+	}
+	
+
+}
